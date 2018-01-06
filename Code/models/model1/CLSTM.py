786479d75ca79946b2520b28c5cfb48aca0a0bbe
Ankitesh Gupta <ankiteshguptas@gmail.com>
2018-01-06 16:27:36 +0530

    CLSTM working

--- Code/models/model1/CLSTM.py
+++ Code/models/model1/CLSTM.py
@@ -1,10 +1,18 @@
 
+
+def load_src(name, fpath):
+    import os, imp
+    return imp.load_source(name, os.path.join(os.path.dirname(__file__), fpath))
+
+load_src("util", "../../utils/model1.py")
+load_src("loadData", "../../utils/loadData.py")
+
+
 import tensorflow as tf
 from CNN import CNN
 from LSTM import LSTM
 from util import *
 from loadData import *
-
 
 #defines the train operation
 def train(onehot_labels, predicted, learning_rate):
@@ -23,17 +31,19 @@ def main():
     inp_shape = config['CNN']['Input_shape']
     time_steps = config['LSTM']['time_steps']
 
-    sequence = tf.placeholder(tf.float32, shape=[time_steps, None]+inp_shape)
-    target = tf.placeholder(tf.float32, shape=[None, config["LSTM"]["vocab_size"]])
-
-    cnn_forward_op = lambda input_tensor: CNN(input_tensor,config["CNN"]).forward
+    sequence = tf.placeholder(tf.float32, shape=[time_steps, None]+inp_shape, name="input_seq")
+    target = tf.placeholder(tf.float32, shape=[None, config["LSTM"]["vocab_size"]], name="target")
+    is_train = tf.placeholder(tf.bool)
+    cnn_forward_op = lambda input_tensor: CNN(input_tensor,config["CNN"],is_training=is_train).forward
     cnn_output = tf.map_fn(cnn_forward_op, sequence, dtype=tf.float32, swap_memory=True)
-    lstm_op_forward = LSTM(config["LSTM"], cnn_output)
+    cnn_output = tf.transpose(cnn_output, [1, 0, 2])
+
+    lstm_op_forward = LSTM(cnn_output,config["LSTM"]).forward
 
     nb_classes = training_params['nb_classes']
     learning_rate = training_params['learning_rate']
 
-    onehot_labels = tf.placeholder(tf.int32, [None, nb_classes])
+    onehot_labels = tf.placeholder(tf.int32, [None, nb_classes], name="onehot")
 
     #prediction
     probabilities = tf.nn.softmax(lstm_op_forward, name="predict")
@@ -46,9 +56,9 @@ def main():
     batch_size = training_params['batch_size']
     acc_size = training_params['acc_size']
 
-    saver = tf.train.Saver()
+    saver = tf.train.Saver(max_to_keep=4)
     init = tf.global_variables_initializer()
-    
+
     with tf.Session() as sess:
 
         sess.run(init)
@@ -56,34 +66,43 @@ def main():
         for e in range(epochs):
             total = loadDataQueue(is_colored)
             if(e%5==0):
-                with open("controlTraining.txt",'r') as f:
-                control = f.read()
-                if control.strip() == "1":
-                   print "-----------------stopping the training process .........."
-                   break
+                with open("control.txt",'r') as f:
+                    control = f.read()
+                    if control.strip() == "1":
+                       print "-----------------stopping the training process .........."
+                       break
             for i in range(0, total, batch_size):
                 X,y = getNextBatch(batch_size, is_colored)
+                y = y.reshape(-1)
                 one_hot_targets = np.eye(nb_classes)[y]
-                sess.run(train_op, feed_dict={sequence:X, target:y})
+                sess.run(train_op, feed_dict={sequence:X,onehot_labels:one_hot_targets, is_train:True})
+            saver.save(sess, './trained_models/clstmModel', global_step=e,write_meta_graph=False)
 
+            emptyDataQueue()
             loadDataQueue(is_colored)
 
             #take data
             X_acc,Y = getNextBatch(acc_size, is_colored)
+            Y = Y.reshape(-1)
             one_hot_targets = np.eye(nb_classes)[Y]
 
-            predictions = sess.run(probabilities, feed_dict={sequence:X_acc})
-            predicted_classes = sess.run(classes, feed_dict={sequence:X_acc, onehot_labels:one_hot_targets})
-            loss_val = sess.run(loss, accuracy,feed_dict={sequence:X_acc, onehot_labels:one_hot_targets})
+            predictions = sess.run(probabilities, feed_dict={sequence:X_acc,is_train:False})
+            predicted_classes = sess.run(classes, feed_dict={sequence:X_acc, onehot_labels:one_hot_targets,is_train:False})
+            loss_val = sess.run(loss,feed_dict={sequence:X_acc, onehot_labels:one_hot_targets,is_train:False})
 
             count=0
             for i in range(len(Y)):
-                if preds_classes[i] == Y[i]:
+                if predicted_classes[i] == Y[i]:
                     count = count + 1
 
-            acc_train = count*(1.0)/len(preds_classes)
+            acc_train = (count*(1.0)/len(predicted_classes)) *100
+            emptyDataQueue()
 
             print "Epoch: "+str(e)+" Loss: "+str(loss_val)+" Train Accuracy: "+str(acc_train)+"%"
 
+        saver.save(sess, "./trained_models/clstmModel_final")
+
 
     #define rnn operation
+
+main()
