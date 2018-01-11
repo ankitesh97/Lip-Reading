6b64512e30b2086be3c6d54095765149c520456a
Ankitesh Gupta <ankiteshguptas@gmail.com>
2018-01-11 15:19:53 +0530

    working model.py

--- Code/models/model2/model.py
+++ Code/models/model2/model.py
@@ -17,15 +17,18 @@ import time
 import sys
 
 
-def cnn_forward(input_array, graph, sess2):
+CONFIG_FILE = 'config_prod.json'
+
+dynamic_config = loadConfig(CONFIG_FILE)
+train_flag = dynamic_config['Training']['is_training']
+model_save_add = dynamic_config["Training"]['save_file_address']
+
+def cnn_forward(input_array,sess2, op, input_te, is_train):
     # input_array dim : 29 X batchSize X width x height x channel
-    graph = tf.get_default_graph()
-    input_te = graph.get_tensor_by_name("Placeholder:0")
-    is_train = graph.get_tensor_by_name("Placeholder_1:0")
     output = []
     for i in range(input_array.shape[0]):
         # print "cool"
-        output.append(sess2.run('cnn_forward/cnn_final_layer:0',feed_dict={input_te:input_array[i], is_train:False}))
+        output.append(sess2.run(op,feed_dict={input_te:input_array[i], is_train:False}))
     return np.array(output)
 
 def tryc():
@@ -73,8 +76,9 @@ def main():
     time_steps = config['LSTM']['time_steps']
     input_shape = config["CNN"]["Input_shape"]
 
-    sequence = tf.placeholder(tf.float32, shape=[time_steps,None, 512], name="Input_seq")
-    is_train = tf.placeholder(tf.bool, name="is_train")
+    sequence = tf.placeholder(tf.float32, shape=[time_steps,None, 512], name="Input_seq_hybrid")
+    is_train = tf.placeholder(tf.bool, name="is_train_hybrid")
+    # cnn_op_
     last_layers = lambda input_tensor: dense_layer_op(input_tensor, is_train)
     last_layers_op = tf.map_fn(last_layers, sequence,  dtype=tf.float32, swap_memory=True)
     last_layers_output = tf.transpose(last_layers_op, [1, 0, 2])
@@ -85,12 +89,12 @@ def main():
     learning_rate = training_params['learning_rate']
 
     onehot_labels = tf.placeholder(tf.int32, [None, nb_classes], name="onehot")
+    train_op, loss = train(onehot_labels, lstm_op_forward, learning_rate)
 
     #prediction
     probabilities = tf.nn.softmax(lstm_op_forward, name="predict")
     classes = tf.argmax(probabilities, axis=1)
 
-    train_op, loss = train(onehot_labels, lstm_op_forward, learning_rate)
 
     epochs = training_params['epochs']
     is_colored = training_params['is_colored']
@@ -101,15 +105,26 @@ def main():
     Losses = []
     model_save_add_local = './trained_models/modelv1/'
 
+    #load trained model
+    # sess = tf.Session()
+    # sess.close()
+
     with tf.Session() as sess:
         new_saver = tf.train.import_meta_graph(model_save_add_local+'clstmModel_final.meta')
         new_saver.restore(sess, tf.train.latest_checkpoint(model_save_add_local))
         graph = tf.get_default_graph()
+        input_te_cnn = graph.get_tensor_by_name("Placeholder:0")
+        is_train_cnn = graph.get_tensor_by_name("Placeholder_1:0")
+        cnn_forward_op = graph.get_tensor_by_name('cnn_forward/cnn_final_layer:0')
+        # print "start"
+        # print graph.get_tensor_by_name('cnn_forward/conv2d/bias:0').eval()
+
+        tf.stop_gradient(cnn_forward_op)
+        sess.run(init)
         print "---------------Starting to train-------------"
         sys.stdout.flush()
-        # tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='my_scope')
-        sess.run(init)
         writer = tf.summary.FileWriter('logs/model2', graph=tf.get_default_graph())
+
         for e in range(epochs):
             total = loadDataQueue(is_colored)
             if(e%3==0):
@@ -123,7 +138,7 @@ def main():
             for i in range(0, total, batch_size):
                 X,y = getNextBatch(batch_size, is_colored)
                 X = X/255.0
-                X = cnn_forward(X, graph, sess)
+                X = cnn_forward(X,sess,cnn_forward_op, input_te_cnn, is_train_cnn)
                 # print "cnn done"
                 y = y.reshape(-1)
                 one_hot_targets = np.eye(nb_classes)[y]
@@ -133,8 +148,8 @@ def main():
                     print "Epoch: "+str(e)+" Batch: "+str(batch_count)
                     sys.stdout.flush()
             if(e%3==0):
-                pass
-                # saver.save(sess, model_save_add+'clstmModel', global_step=e,write_meta_graph=False)
+                new_saver.save(sess, model_save_add+'clstmModel', global_step=e,write_meta_graph=False)
+            # print graph.get_tensor_by_name('cnn_forward/conv2d/bias:0').eval()
 
             emptyDataQueue()
             loadDataQueue(is_colored)
@@ -142,7 +157,7 @@ def main():
             #take data
             X_acc,Y = getNextBatch(acc_size, is_colored)
             X_acc =X_acc/255.0
-            X_acc = cnn_forward(X_acc, graph, sess)
+            X_acc = cnn_forward(X_acc,sess,cnn_forward_op, input_te_cnn, is_train_cnn)
             Y = Y.reshape(-1)
             one_hot_targets = np.eye(nb_classes)[Y]
 
@@ -160,12 +175,14 @@ def main():
 
             print "Epoch: "+str(e)+" Loss: "+str(loss_val)+" Train Accuracy: "+str(acc_train)+"%"+" Count "+str(count)
             sys.stdout.flush()
-
-        # saver.save(sess, model_save_add+"clstmModel_final")
-        print Losses
+    #
+        new_saver.save(sess, model_save_add+"clstmModel_final")
         open("losses.txt", "w").write(json.dumps({"losses":map(float,Losses)}))
 
-    #define rnn operation
+
+
+
+def test():
 
 main()
 # tryc()
