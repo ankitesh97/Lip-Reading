6ed8b7ba6904bf2d1c1f2b6e7d0cf1339e3ea07a
Ankitesh Gupta <ankiteshguptas@gmail.com>
2018-01-11 19:40:29 +0530

    model 2 working

--- Code/models/model2/model.py
+++ Code/models/model2/model.py
@@ -23,6 +23,30 @@ dynamic_config = loadConfig(CONFIG_FILE)
 train_flag = dynamic_config['Training']['is_training']
 model_save_add = dynamic_config["Training"]['save_file_address']
 
+
+class ImportGraph():
+    """  Importing and running isolated TF graph """
+    def __init__(self, loc):
+        # Create local graph and use it in the session
+        self.graph = tf.Graph()
+        self.sess = tf.Session(graph=self.graph)
+        with self.graph.as_default():
+            # Import saved model from location 'loc' into local graph
+            saver = tf.train.import_meta_graph(loc + '.meta',
+                                               clear_devices=True)
+            saver.restore(self.sess, loc)
+            # Get activation function from saved collection
+            # You may need to change this in case you name it differently
+            self.op = "cnn_forward/cnn_final_layer:0"
+
+    def run(self, data):
+        """ Running the activation function previously imported """
+        # The 'x' corresponds to name of input placeholder
+        output = []
+        for i in range(data.shape[0]):
+            output.append(self.sess.run(self.op, feed_dict={"Placeholder:0": data[0], "Placeholder_1:0":False}))
+        return np.array(output)
+
 def cnn_forward(input_array,sess2, op, input_te, is_train):
     # input_array dim : 29 X batchSize X width x height x channel
     output = []
@@ -30,6 +54,7 @@ def cnn_forward(input_array,sess2, op, input_te, is_train):
         # print "cool"
         output.append(sess2.run(op,feed_dict={input_te:input_array[i], is_train:False}))
     return np.array(output)
+
 
 def tryc():
     total = loadDataQueue(0)
@@ -76,9 +101,9 @@ def main():
     time_steps = config['LSTM']['time_steps']
     input_shape = config["CNN"]["Input_shape"]
 
+
     sequence = tf.placeholder(tf.float32, shape=[time_steps,None, 512], name="Input_seq_hybrid")
     is_train = tf.placeholder(tf.bool, name="is_train_hybrid")
-    # cnn_op_
     last_layers = lambda input_tensor: dense_layer_op(input_tensor, is_train)
     last_layers_op = tf.map_fn(last_layers, sequence,  dtype=tf.float32, swap_memory=True)
     last_layers_output = tf.transpose(last_layers_op, [1, 0, 2])
@@ -92,7 +117,7 @@ def main():
     train_op, loss = train(onehot_labels, lstm_op_forward, learning_rate)
 
     #prediction
-    probabilities = tf.nn.softmax(lstm_op_forward, name="predict")
+    probabilities = tf.nn.softmax(lstm_op_forward, name="predict_hybrid")
     classes = tf.argmax(probabilities, axis=1)
 
 
@@ -103,24 +128,15 @@ def main():
     # saver = tf.train.Saver(max_to_keep=4)
     init = tf.global_variables_initializer()
     Losses = []
-    model_save_add_local = './trained_models/modelv1/'
+    model_save_add_local = './trained_models/modelv1/clstmModel_final'
 
-    #load trained model
-    # sess = tf.Session()
-    # sess.close()
+    cnn_model = ImportGraph(model_save_add_local)
 
+    saver = tf.train.Saver(max_to_keep=4)
     with tf.Session() as sess:
-        new_saver = tf.train.import_meta_graph(model_save_add_local+'clstmModel_final.meta')
-        new_saver.restore(sess, tf.train.latest_checkpoint(model_save_add_local))
-        graph = tf.get_default_graph()
-        input_te_cnn = graph.get_tensor_by_name("Placeholder:0")
-        is_train_cnn = graph.get_tensor_by_name("Placeholder_1:0")
-        cnn_forward_op = graph.get_tensor_by_name('cnn_forward/cnn_final_layer:0')
-        # print "start"
-        # print graph.get_tensor_by_name('cnn_forward/conv2d/bias:0').eval()
-
-        tf.stop_gradient(cnn_forward_op)
         sess.run(init)
+        graph = tf.get_default_graph()
+
         print "---------------Starting to train-------------"
         sys.stdout.flush()
         writer = tf.summary.FileWriter('logs/model2', graph=tf.get_default_graph())
@@ -138,8 +154,8 @@ def main():
             for i in range(0, total, batch_size):
                 X,y = getNextBatch(batch_size, is_colored)
                 X = X/255.0
-                X = cnn_forward(X,sess,cnn_forward_op, input_te_cnn, is_train_cnn)
-                # print "cnn done"
+                X = cnn_model.run(X)
+
                 y = y.reshape(-1)
                 one_hot_targets = np.eye(nb_classes)[y]
                 sess.run(train_op, feed_dict={sequence:X,onehot_labels:one_hot_targets, is_train:True})
@@ -148,8 +164,8 @@ def main():
                     print "Epoch: "+str(e)+" Batch: "+str(batch_count)
                     sys.stdout.flush()
             if(e%3==0):
-                new_saver.save(sess, model_save_add+'clstmModel', global_step=e,write_meta_graph=False)
-            # print graph.get_tensor_by_name('cnn_forward/conv2d/bias:0').eval()
+                saver.save(sess, model_save_add+'model', global_step=e,write_meta_graph=False)
+
 
             emptyDataQueue()
             loadDataQueue(is_colored)
@@ -157,7 +173,7 @@ def main():
             #take data
             X_acc,Y = getNextBatch(acc_size, is_colored)
             X_acc =X_acc/255.0
-            X_acc = cnn_forward(X_acc,sess,cnn_forward_op, input_te_cnn, is_train_cnn)
+            X_acc = cnn_model.run(X_acc)
             Y = Y.reshape(-1)
             one_hot_targets = np.eye(nb_classes)[Y]
 
@@ -176,13 +192,57 @@ def main():
             print "Epoch: "+str(e)+" Loss: "+str(loss_val)+" Train Accuracy: "+str(acc_train)+"%"+" Count "+str(count)
             sys.stdout.flush()
     #
-        new_saver.save(sess, model_save_add+"clstmModel_final")
+
+        saver.save(sess, model_save_add+"model_final")
+        print graph.get_tensor_by_name('rnn_forward/dense_1/bias:0').eval()
+
         open("losses.txt", "w").write(json.dumps({"losses":map(float,Losses)}))
 
 
 
 
 def test():
+    c = loadConfig(CONFIG_FILE)
+    nb_classes = c["Training"]["nb_classes"]
+    new_saver = tf.train.import_meta_graph(model_save_add+'model_final.meta')
+    graph = tf.get_default_graph()
 
-main()
-# tryc()
+    model_save_add_local = './trained_models/modelv1/clstmModel_final'
+
+    cnn_model = ImportGraph(model_save_add_local)
+
+    with tf.Session() as sess:
+
+
+        new_saver.restore(sess, tf.train.latest_checkpoint(model_save_add))
+        sequence = graph.get_tensor_by_name('Input_seq_hybrid:0')
+        is_train = graph.get_tensor_by_name('is_train_hybrid:0')
+        onehot_labels = graph.get_tensor_by_name('onehot:0')
+
+        total = loadDataQueue()
+        X, y = getNextBatch(total)
+        X = X/255.0
+        X = cnn_model.run(X)
+        y2 = y.reshape(-1)
+        one_hot_targets = np.eye(nb_classes)[y2]
+
+        preds =  sess.run('predict_hybrid:0',feed_dict={sequence:X, is_train:False, onehot_labels:one_hot_targets})
+        classes = tf.argmax(preds, axis=1)
+        preds_classes = sess.run(classes)
+        print len(preds_classes)
+        print preds_classes
+        print "actual"
+        print len(y)
+        print y
+        count=0
+        for i in range(len(preds_classes)):
+            if preds_classes[i] == y[i]:
+                count = count + 1
+        print count
+        print ((count*1.0)/len(preds_classes)) * 100
+
+if __name__ == '__main__':
+    if train_flag:
+        main()
+    else:
+        test()
