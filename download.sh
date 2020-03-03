449171c7867db0d17765aacb4ea4bea48b128db4
Ankitesh Gupta <ankiteshguptas@gmail.com>
2020-03-02 20:12:51 -0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget --user user064 --password xG7Kcamsep http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partaa
-wget --user user064 --password xG7Kcamsep http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partab
-wget --user user064 --password xG7Kcamsep http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partac
-wget --user user064 --password xG7Kcamsep http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partad
-wget --user user064 --password xG7Kcamsep http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partae
-wget --user user064 --password xG7Kcamsep http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partaf
-wget --user user064 --password xG7Kcamsep http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partag
+wget --user user064 --password **** http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partaa
+wget --user user064 --password **** http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partab
+wget --user user064 --password **** http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partac
+wget --user user064 --password **** http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partad
+wget --user user064 --password **** http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partae
+wget --user user064 --password **** http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partaf
+wget --user user064 --password **** http://www.robots.ox.ac.uk/~vgg/data/lip_reading/data1/lrw-v1-partag
